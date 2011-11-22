5229ab0ffe1e39d6a777f43ac5c7c1e20915f043
Jennifer Smith <jennifer@home>
2011-11-22 07:00:23 -0600

    Forgot to make sure the actual gem would work - missing require. Why is this not tested?

--- lib/wire_tap/configuration.rb
+++ lib/wire_tap/configuration.rb
@@ -1,14 +1,15 @@
+require 'main'
 module WireTap
-	class << self
-		wire_tap = WireTap::Main.new
-		def start 
-			wire_tap.start
-		end
-		def dump
-			wire_tap.dump
-		end
-		def logger
-			wire_tap.logger
-		end
-	end
+  class << self
+    wire_tap = WireTap::Main.new
+    def start 
+      wire_tap.start
+    end
+    def dump
+      wire_tap.dump
+    end
+    def logger
+      wire_tap.logger
+    end
+  end
 end
